47cd66c6995cd61246cb1c2c879b743c50be45ce
jay75chauhan <chauhan75jayk@gmail.com>
2021-11-08 14:28:32 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+
+import resolvers from "./resolvers";
+
+import schema from "./schema";
+
+import { graphqlHTTP } from "express-graphql";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("yaa we are runing");
+});
+
+const root = resolvers;
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+app.listen(8000, () => console.log("running at 8000"));
---
+++ resolvers.js
@@ -0,0 +1,33 @@
+import { nanoid } from "nanoid";
+
+class Course {
+  constructor(
+    id,
+    { courseName, category, price, language, email, stack, techingAssists }
+  ) {
+    this.id = id;
+    this.courseName = courseName;
+    this.category = category;
+    this.price = price;
+    this.language = language;
+    this.email = email;
+    this.stack = stack;
+    this.techingAssists = techingAssists;
+  }
+}
+
+const courseholder = {};
+
+const resolvers = {
+  getCourse: ({ id }) => {
+    return new Course(id, courseholder[id]);
+  },
+
+  createCourse: ({ input }) => {
+    let id = nanoid();
+    courseholder[id] = input;
+    return new Course(id, input);
+  },
+};
+
+export default resolvers;
